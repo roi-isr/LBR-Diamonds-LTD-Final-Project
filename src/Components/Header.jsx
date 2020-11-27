f0cbf9f5a8eb0d053d9ce126404eec13998a6e2c
Roi Israeli <roiisra4@gmail.com>
2020-11-27 13:21:37 +0200

    small changes UI commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,46 +1,31 @@
 import React from 'react';
 import "./Header.css"
 import Logo from '../Assets/logo.jpg'
-import Homepage from './Homepage';
-import { Form, FormControl, Button, Nav, NavDropdown, Navbar } from 'react-bootstrap';
-import Avatar from '@material-ui/core/Avatar';
+import { Form, FormControl, Button, Nav, Navbar } from 'react-bootstrap';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const Header = () => {
-
     return (
-
         <header>
-            <img src={Logo} className="logo" alt="logo image" />
+            <img src={Logo} className="logo" alt="logo" />
             <NavItem />
         </header>
-
-    )
-
-}
-
-const LoginBtn = () => {
-    return
-    (
-        <Avatar />
     );
 }
 
+
 const NavItem = () => {
-
     return (
-
         <Navbar bg="light" expand="lg">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto" style={{ justifyContent: "space-between", width: "60%" }}>
-                    <Nav.Link href="#login"><AccountCircleIcon className="login-btn" fontSize="medium" color="blue" /></Nav.Link>
+                <Nav className="mr-auto" style={{ justifyContent: "space-between", width: "60%", margin:"auto" }}>
+                    <Nav.Link href="#login"><AccountCircleIcon className="login-btn" fontSize="default" /></Nav.Link>
                     <Nav.Link href="/home">Home</Nav.Link>
                     <Nav.Link href="/store">Shop</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="/qa">FAQ</Nav.Link>
                     <Nav.Link href="/contact">Contact us</Nav.Link>
-
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -48,11 +33,8 @@ const NavItem = () => {
                 </Form>
             </Navbar.Collapse>
         </Navbar>
-
     )
-
 }
-
 
 
 export default Header;
--- src/Components/VirtualStore.jsx
+++ src/Components/VirtualStore.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 class VirtualStore extends React.Component {
     render() {
       return (

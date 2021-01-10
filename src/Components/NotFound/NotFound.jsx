05f4a5289b8c3c0517a37e9097ce06159139633c
Roi Israeli <roiisra4@gmail.com>
2021-01-10 09:49:17 +0200

    Error 404 page added

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import styled from 'styled-components/macro'
+import NotFoundPic from '../../Assets/not_found_404/not_found_img.jpg'
 
 const NotFoundMsg = styled.h1`
     text-align: center;
     width: 100%;
-    font-size: 250px;
+    font-size: 100px;
+    text-shadow: 2px 2px 2px #000000;
+`
+const NotFoundImg = styled.img`
+    margin: auto;
+    width: 400px;
+    height: 400px;
 `
 
 class PageNotFound extends React.Component {
   render() {
     return (
-      <NotFoundMsg>Error 404 :(</NotFoundMsg>
+      <div style={{textAlign:'center'}}>
+        <NotFoundMsg>Page Not Found (Error 404) :(</NotFoundMsg>
+        <NotFoundImg src={NotFoundPic} alt="not_found" />
+      </div>
     );
   }
 }

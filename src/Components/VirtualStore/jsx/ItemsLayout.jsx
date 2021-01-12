9fe4e0bafee22ce04c852ae9915fce5e3e77a934
Roi Israeli <roiisra4@gmail.com>
2021-01-12 12:58:30 +0200

    Merge virtual store prototype

---
+++ src/Components/VirtualStore/jsx/ItemsLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+
+  const VsIcons = styled.img`
+
+`;
+
+function ItemLayout(props) {
+  
+}
+
+
+
+export default ItemsLayout;
--- src/Components/VirtualStore/jsx/VirtualStore.jsx
+++ src/Components/VirtualStore/jsx/VirtualStore.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import '../css/VirtualStore.css';
+import styled from 'styled-components/macro';
+import DiamondIcon from '../../../Assets/virtual_store_icons/diamond.png'
+import PackageIcon from '../../../Assets/virtual_store_icons/package.png'
+
+const NavBtn = styled.button`
+  margin: auto;
+  width: 50%;
+  height: 50%;
+  font-size: 50px;
+  background-color:palegreen;
+  margin-bottom: 25%;
+  @media (max-width: 910px){
+    width:100%; 
+    margin-bottom: 0%;
+    margin-top: 3%;
+  }
+  `;
+
+const VsIcons = styled.img`
+  height: 100px;
+  width: 100px;
+  float: left;
+`;
+
+function StoreNav(props) {
+  const verticalLine =
+    <div className="vertical-line-vs" />
+  const navItems = [
+    { name: 'חבילות', icon: PackageIcon },
+    { name: 'אבנים מתועדות', icon: DiamondIcon }
+  ];
+  return (
+    <div className="selection-vs">
+      {navItems.map((item, index) =>
+        <NavBtn
+          key={index}
+          onClick={(index) => props.applyLayout(index)}>
+          {item.name}
+          <VsIcons src={item.icon} />
+        </NavBtn>
+      )}
+    </div>
+  );
+}
+
 
 class VirtualStore extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+      layout: ""
+    }
+  }
+  applyLayout = () => {
+
+  }
   render() {
     return (
-      <div>Virtual Store</div>
+      <StoreNav />
     );
   }
 }

ebcdc96e35db4730a7a07d9be99b35901460054a
Daniel Yedid <dyedid08@gmail.com>
2021-01-16 22:40:41 +0200

    tables form

--- src/Components/DeliveryCheck/jsx/DeliveryFile.jsx
+++ src/Components/DeliveryCheck/jsx/DeliveryFile.jsx
@@ -1,62 +1,162 @@
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
-import '../css/DeliveryFile.css';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
+import Icon from '@material-ui/core/Icon';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-const columns = [
-  { field: 'id', headerName: 'מספר החבילה', width: 300, textAlign: 'right', headerAlign: 'center', align: 'center' },
-  { field: 'weigth', type: "number", headerName: 'משקל החבילה', width: 300, textAlign: 'right', headerAlign: 'center', align: 'center' },
-  { field: 'from_country', headerName: 'מהיכן המשלוח', width: 300, textAlign: 'right', headerAlign: 'center', align: 'center' },
-  { field: 'delivery_comany', headerName: 'חברת השילוח', width: 300, textAlign: 'right', headerAlign: 'center', align: 'center' },
-  {
-    field: 'name', headerName: 'שם השולח ', width: 300, textAlign: 'right', headerAlign: 'center', align: 'center',
+import Fab from '@material-ui/core/Fab';
+
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
   },
+});
 
-];
-
-const rows = [
-  { id: 1, weigth: 123.5, from_country: 'israel', delivery_comany: 'malca amit', name: 'daniel' },
-  { id: 2, weigth: 123.5, from_country: 'china', delivery_comany: 'malca amit', name: 'yoav' },
-  { id: 3, weigth: 123.5, from_country: 'russia', delivery_comany: 'fedex', name: 'amir' },
-  { id: 4, weigth: 123.5, from_country: 'arg', delivery_comany: 'malca amit', name: 'roi' },
-  { id: 5, weigth: 123.5, from_country: 'usa', delivery_comany: 'malca amit', name: 'nissim' },
-  { id: 6, weigth: 123.5, from_country: 'israel', delivery_comany: 'malca amit', name: 'daniel' },
-  { id: 7, weigth: 123.5, from_country: 'china', delivery_comany: 'malca amit', name: 'yoav' },
-  { id: 8, weigth: 123.5, from_country: 'russia', delivery_comany: 'fedex', name: 'amir' },
-  { id: 9, weigth: 123.5, from_country: 'arg', delivery_comany: 'malca amit', name: 'roi' },
-  { id: 10, weigth: 123.5, from_country: 'usa', delivery_comany: 'malca amit', name: 'nissim' },
-  { id: 11, weigth: 123.5, from_country: 'israel', delivery_comany: 'malca amit', name: 'daniel' },
-  { id: 12, weigth: 123.5, from_country: 'china', delivery_comany: 'malca amit', name: 'yoav' },
-  { id: 13, weigth: 123.5, from_country: 'russia', delivery_comany: 'fedex', name: 'amir' },
-  { id: 14, weigth: 123.5, from_country: 'arg', delivery_comany: 'malca amit', name: 'roi' },
-  { id: 15, weigth: 123.5, from_country: 'usa', delivery_comany: 'malca amit', name: 'nissim' },
-  { id: 16, weigth: 123.5, from_country: 'israel', delivery_comany: 'malca amit', name: 'daniel' },
-  { id: 17, weigth: 123.5, from_country: 'china', delivery_comany: 'malca amit', name: 'yoav' },
-  { id: 18, weigth: 123.5, from_country: 'russia', delivery_comany: 'fedex', name: 'amir' },
-  { id: 19, weigth: 123.5, from_country: 'arg', delivery_comany: 'malca amit', name: 'roi' },
-  { id: 20, weigth: 123.5, from_country: 'usa', delivery_comany: 'malca amit', name: 'nissim' },
-
-
-];
-
-export default function DeliveryTable() {
-  return (
-    <React.Fragment>
-      <div className="delivery-table-wrapper">
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pnameSize={10}
-          checkboxSelection
-        />
-      </div>
-      <div className="credit-util-div" >
-        <label>ניצול מסגרת אשראי </label>
-        <CircularProgressbar
-          percentage={10}
-          text={`${10}%`}
-        />
-      </div>
-    </React.Fragment>
-  );
+function createData(...data) {
+  const [id, weigth, from_country, delivery_comany, contact_name, date] = data
+  return { id, weigth, from_country, delivery_comany, contact_name, date };
 }
+
+
+const englishTitles = ['id', 'weigth', 'from_country', 'delivery_comany', 'contact_name', 'date'];
+const titles = ["מספר החבילה", "משקל החבילה", "מהיכן המשלוח", "חברת השילוח", "שם השולח ", "תאריך המשלוח"];
+
+
+const rowsData = [
+  createData("R101", 15, 'ISRAEL', 'Fedex', 'דניאל', '1.1.21'),
+
+  createData("R101", 15, 'ISRAEL', 'Fedex', 'דניאל', '1.1.21'),
+
+  createData("R101", 15, 'ISRAEL', 'Fedex', 'דניאל', '1.1.21'),
+
+
+];
+
+export default function SellTable() {
+  const classes = useStyles();
+
+  const [rows, setRows] = useState([...rowsData]);
+
+  const deleteBtn = (ind) => {
+    const con = window.confirm("Are you sure?");
+    if (!con)
+      return;
+    setRows(currRow =>
+      currRow.filter((item, index) => index !== ind));
+  }
+  const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+    
+    const add_form = () => {
+      return (
+        <div>
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            Open form dialog
+        </Button>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To subscribe to this website, please enter your email address here. We will send updates
+                occasionally.
+            </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Email Address"
+                type="email"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+              <Button onClick={handleClose} color="primary">
+                Subscribe
+            </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      );
+
+    }
+  
+
+
+    return (
+      <React.Fragment>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table" style={{ direction: 'rtl' }}>
+            <TableHead>
+              <TableRow>
+                {titles.map((item, index) => (
+                  <TableCell key={index} align="left">
+                    {item}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={row.weigth}>
+                  <TableCell
+                    key={index}
+                    component="th"
+                    scope="row">
+                    {row.id}
+                  </TableCell>
+                  {Object.keys(row)
+                    // .filter((filItem) => filItem !== 'name')
+                    .map((item, index) =>
+                      <TableCell
+                        key={index}
+                        align="left">
+                        {row[englishTitles[index]]}
+                      </TableCell>
+                    )}
+
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    onClick={deleteBtn}
+                    className={"delete_btn"}
+
+                  >
+                    אישור הגעה
+      </Button>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Fab color="primary" aria-label="add" onClick={add_form}>
+          <AddIcon />
+        </Fab>
+      </React.Fragment>
+    );
+  }
